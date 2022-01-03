6e685611326581b1c8af7fbb3bd3496724743d31
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-01-03 11:37:44 +0100

    Adjust urls

--- download_packages.sh
+++ download_packages.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 #Versions
-hestia="1.5.0~beta"
-hestianginx="1.21.3"
-hestiaphp="7.4.25"
+hestia="1.5.3~alpha"
+hestianginx="1.21.5"
+hestiaphp="7.4.27"
 
 #OS 
 os=$(lsb_release -s -i | tr '[:upper:]' '[:lower:]');
 version=$(lsb_release -s -r | tr '[:upper:]' '[:lower:]' | sed 's/\.//g');
+codename="$(lsb_release -s -c)"
 aarch=$(dpkg --print-architecture)
 
 echo "OS: $os"
@@ -15,6 +16,6 @@ echo "Version: $version"
 echo "Architecture: $aarch"
 
 #Download packages 
-wget "https://apt.hestiacp.com/beta/${os}-${version}/hestia_${hestia}_${aarch}.deb"
-wget "https://apt.hestiacp.com/beta/${os}-${version}/hestia-nginx_${hestianginx}_${aarch}.deb"
-wget "https://apt.hestiacp.com/beta/${os}-${version}/hestia-php_${hestiaphp}_${aarch}.deb"
+wget "https://apt.hestiacp.com/beta/${os}/${codename}/hestia_${hestia}_${aarch}.deb"
+wget "https://apt.hestiacp.com/beta/${os}/${codename}/hestia-nginx_${hestianginx}_${aarch}.deb"
+wget "https://apt.hestiacp.com/beta/${os}/${codename}/hestia-php_${hestiaphp}_${aarch}.deb"
