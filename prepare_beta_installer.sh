4e170dbb8ff2a53c46188889a22c8788527381f2
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-06-28 13:05:56 +0200

    Update comments

--- prepare_beta_installer.sh
+++ prepare_beta_installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-# Download installer of a random branch and add beta repo info
+# Download installers of a branch and add beta repo info
+# Usage ./prepare_beta_installer.sh hestiacp beta/1.6.0 
 
 user=$1
 branch=$2
@@ -22,6 +22,7 @@ for installer in debian ubuntu; do
     # Remove development check
     # Note: Also remvoves non supported Architecture
     sed -i '/check_result 1 "Installation aborted"/d' hst-install-$installer.sh
+    # Disables apt.hestiacp.com and add beta.hestiacp.com apt.hestiacp.com key is added to the system allowing easier switching
     sed -i 's/deb \[arch=\$ARCH signed-by=\/usr\/share\/keyrings\/hestia-keyring\.gpg\]/#deb \[arch=\$ARCH signed-by=\/usr\/share\/keyrings\/hestia-keyring\.gpg\]/g' hst-install-$installer.sh
     sed -i '/# Installing HestiaCP repo/a insert-line2'  hst-install-$installer.sh
     sed -i '/# Installing HestiaCP repo/a insert-line1'  hst-install-$installer.sh
